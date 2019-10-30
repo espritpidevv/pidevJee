6273fdc99281db10def0a459c5c50cab2450f3e7
nesriaUS <ASUS@DESKTOP-VGJ613Q>
2019-10-30 21:43:29 +0100

    first commit nesria

---
+++ pidev/pidev-ejb/src/main/java/tn/esprit/entity/Critere.java
@@ -0,0 +1,64 @@
+package tn.esprit.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+@Entity
+public class Critere implements Serializable{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+private int id ;
+private String niveau ;
+private String description ;
+public String getDescription() {
+	return description;
+}
+public void setDescription(String description) {
+	this.description = description;
+}
+@ManyToOne
+@JoinColumn(name="emlployeid")
+private Evaluation evaluation  ;
+
+public Critere() {
+	super();
+}
+public Critere( String niveau2,String descri) {
+this.niveau=niveau2;
+this.description=descri;
+}
+public Critere(String description2, String niveau2, int setIdBeUpdate) {
+this.description=description2;
+this.niveau=niveau2;
+this.id=setIdBeUpdate;}
+
+@Override
+public String toString() {
+	return description;
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getNiveau() {
+	return niveau;
+}
+public void setNiveau(String niveau) {
+	this.niveau = niveau;
+}
+public Evaluation getEvaluation() {
+	return evaluation;
+}
+public void setEvaluation(Evaluation evaluation) {
+	this.evaluation = evaluation;
+} 
+
+}
